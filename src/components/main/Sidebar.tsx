5f95bbdcee21cd63f7b8295ba628980ed35dca0d
SeHIgh <sehi0119@naver.com>
2025-04-20 17:47:04 +0900

    feat(sidebar):  기본 레이아웃 - 사이드바 제작 1-1. Mac finder의 접고 닫히는 Collapsible & 평소 안보이다가 마우스 hover 시 액션 버튼이 나타나도록 디자인 1-2. 보통 본인을 소개하는 경우 'About'을 많이 쓰는 것으로 보여 Profile -> About 으로 전환 2-1. 그룹을 나누어 관리 'Pinned', 'Tags' 2-2. 'Tags' 그룹은 관심있는 태그 주제에 빠르게 접근하도록 nav 목록을 제공 2-3. 일반적으로 라이브러리 Docs에서 사용하는 업데이트 로그를 차용
    'DEV LOG' 라는 nav를 넣어 짧게 한 문장 정도로 작업한 내용 추가 예정
    3-1. 평소 안보이다가 마우스 hover 시 액션 버튼이 나타나도록 디자인
    3-2. 해당 디자인을 추후에 활용할 수 있도록 커스텀 컴포넌트로 재작성
    4-1. 동그라미 아이콘 적용 및 fill 색상으로 구분 (미리 tag에 대해 할당)
    
    (추가 내용)
    *-1. 헤더 - 프로필 추가 (사진 & 이름 & 한줄 소개)
    
    *-2. 푸터 - 경로 추가 및 할당 아이콘 적용 (각 경로 클릭 시 해당 페이지로 이동)

--- src/components/main/Sidebar.tsx
+++ src/components/main/Sidebar.tsx
@@ -2,83 +2,74 @@
 
 import { cn } from "@/lib/utils";
 import { ArrowLeftIcon } from "lucide-react";
-import Link from "next/link";
 import { useState } from "react";
-import { categoryIconMap } from "../IconMap";
-
-interface SideListProps {
-    isOpen: boolean;
-    title: string;
-    Icon: React.ComponentType<{ className?: string }>;
-}
-
-export const SideList: React.FC<SideListProps> = ({ isOpen, title, Icon }) => {
-    return (
-        // 접을 때 아이콘이 이동하는 현상 발생
-        // 아이콘이 고정 되게 하기 위해 수정
-        <li
-            title={title}
-            className="h-10 pl-2.5 flex justify-start items-center rounded-md transition-all duration-300 ease-in-out hover:bg-slate-400"
-        >
-            <Link
-                href={`/${title}`}
-                passHref
-                className="flex justify-center items-center"
-            >
-                {/* 아이콘 값이 없을 경우 기본 아이콘으로 folder 사용 */}
-                <Icon className="w-5 h-5" />
-                {isOpen && <span className="ml-2">{title.toUpperCase()}</span>}
-                {/* isOpen이 true일 때만 제목 표시 */}
-            </Link>
-        </li>
-    );
-};
+import { pinnedIconMap, tagIconMap } from "../IconMap";
+import { SidebarNavCollapsible } from "../ui/customCollapsible";
 
 export default function Sidebar() {
     const [isOpen, setIsOpen] = useState(true);
-    const categories = [
-        { title: "home" },
-        { title: "docs" },
-        { title: "dev" },
-        { title: "profile" },
-        // ... 추가
+    const [pinnedIsOpen, setPinnedIsOpen] = useState(true);
+    const [tagsIsOpen, setTagsIsOpen] = useState(true);
+
+    const pinnedList = [
+        { title: "home", url: "/" },
+        { title: "about", url: "/about" },
+        { title: "posts", url: "/posts" },
+        { title: "dev.log", url: "/dev.log" },
+        { title: "projects", url: "/projects" },
+    ];
+
+    const tagsList = [
+        { title: "all", url: "/tags/all" },
+        { title: "cs", url: "/tags/cs" },
+        { title: "language", url: "/tags/language" },
+        { title: "algorithm", url: "/tags/algorithm" },
+        { title: "frontend", url: "/tags/frontend" },
+        { title: "backend", url: "/tags/backend" },
+        { title: "review", url: "/tags/review" },
     ];
 
     return (
         <aside
             className={`p-2 bg-background transition-[width] duration-300 ease-in-out overflow-x-hidden overflow-y-auto ${
                 isOpen ? "w-48" : "w-14"
-            } flex flex-col`}
+            } flex flex-col justify-between`}
         >
-            <div className="flex justify-end">
-                <button className="mr-2" onClick={() => setIsOpen(!isOpen)}>
-                    <ArrowLeftIcon
-                        className={cn(
-                            isOpen ? "" : "-rotate-180",
-                            "transition duration-300 ease-in-out"
-                        )}
+            <div className="flex flex-col justify-between gap-5">
+                <div className="flex justify-end">
+                    <button className="mr-2" onClick={() => setIsOpen(!isOpen)}>
+                        <ArrowLeftIcon
+                            className={cn(
+                                isOpen ? "" : "-rotate-180",
+                                "transition duration-300 ease-in-out"
+                            )}
+                        />
+                    </button>
+                </div>
+                {/* // 글 줄바꿈 방지 */}
+                <nav aria-label="sidebar nav" className="truncate">
+                    <h2 className="sr-only">Sidebar Nav</h2>
+                    {/* Pinned 그룹 */}
+                    <SidebarNavCollapsible
+                        open={pinnedIsOpen}
+                        onOpenChange={setPinnedIsOpen}
+                        title="Pinned"
+                        categories={pinnedList}
+                        categoryIconMap={pinnedIconMap}
+                        isOpen={isOpen}
                     />
-                </button>
+                    {/* Tags 그룹 */}
+                    <SidebarNavCollapsible
+                        className="mt-4"
+                        open={tagsIsOpen}
+                        onOpenChange={setTagsIsOpen}
+                        title="Tags"
+                        categories={tagsList}
+                        categoryIconMap={tagIconMap}
+                        isOpen={isOpen}
+                    />
+                </nav>
             </div>
-            {/* {isOpen && ( */}
-            {/* // 글 줄바꿈 방지 */}
-            <nav className="truncate mt-2">
-                <ul className="flex flex-col gap-2">
-                    {categories.map(({ title }) => {
-                        const Icon =
-                            categoryIconMap[title] || categoryIconMap.default;
-                        return (
-                            <SideList
-                                key={title}
-                                isOpen={isOpen}
-                                title={title}
-                                Icon={Icon}
-                            />
-                        );
-                    })}
-                </ul>
-            </nav>
-            {/* )} */}
         </aside>
     );
 }
