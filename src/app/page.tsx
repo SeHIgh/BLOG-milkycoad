94dc3ceac1fc80a224f34358a638eac128f9f659
SeHIgh <sehi0119@naver.com>
2025-04-20 22:35:54 +0900

    feat: 각종 페이지 작성 - home, about, error, 404 page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,100 +1,52 @@
-import MainLogo from "@/assets/svg";
-import Image from "next/image";
-import Link from "next/link";
+import React from "react";
+import { Card, CardTitle, CardContent } from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function Home() {
     return (
-        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center h-full w-full p-2 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-            <main className="flex flex-col justify-center gap-[32px] row-start-2 items-center">
-                <Link href="/" className="flex flex-row gap-1">
-                    <MainLogo className="w-40 h-40" />
-                </Link>
-                <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-                    <li className="mb-2 tracking-[-.01em]">
-                        Get started by editing{" "}
-                        <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-                            src/app/page.tsx
-                        </code>
-                        .
-                    </li>
-                    <li className="tracking-[-.01em]">
-                        Save and see your changes instantly.
-                    </li>
-                </ol>
+        <div className="w-full h-full p-4 text-center flex flex-col justify-between">
+            <h1 className="text-4xl md:text-5xl font-extrabold mb-4">
+                미리내를 잇는 코드
+            </h1>
+            <h2 className="text-3xl text-accent-foreground">
+                SeHi&apos;s Tech Blog
+            </h2>
 
-                <div className="flex gap-4 items-center flex-col sm:flex-row">
-                    <a
-                        className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-                        href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        <Image
-                            className="dark:invert"
-                            src="/vercel.svg"
-                            alt="Vercel logomark"
-                            width={20}
-                            height={20}
-                        />
-                        Deploy now
-                    </a>
-                    <a
-                        className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-                        href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Read our docs
-                    </a>
-                </div>
-            </main>
-            <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-                <a
-                    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                    href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <Image
-                        aria-hidden
-                        src="/file.svg"
-                        alt="File icon"
-                        width={16}
-                        height={16}
-                    />
-                    Learn
-                </a>
-                <a
-                    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                    href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <Image
-                        aria-hidden
-                        src="/window.svg"
-                        alt="Window icon"
-                        width={16}
-                        height={16}
-                    />
-                    Examples
-                </a>
-                <a
-                    className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-                    href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <Image
-                        aria-hidden
-                        src="/globe.svg"
-                        alt="Globe icon"
-                        width={16}
-                        height={16}
-                    />
-                    Go to nextjs.org →
-                </a>
-            </footer>
+            <div className="mt-4 flex-1 space-y-8 text-gray-400 flex flex-col justify-between gap-4">
+                {/* 최근 포스트 섹션 */}
+                <Card className="border-2 border-foreground m-0">
+                    <CardTitle className="text-start px-4">
+                        최근 포스트
+                    </CardTitle>
+                    <CardContent>
+                        <div className="space-y-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2 min-h-50">
+                            {[...Array(4)].map((_, i) => (
+                                <Skeleton
+                                    key={i}
+                                    className="h-full min-h-40 w-full rounded-md"
+                                />
+                            ))}
+                        </div>
+                    </CardContent>
+                </Card>
+
+                {/* 최근 작업 로그 섹션 */}
+                <Card className="border-2 border-foreground flex-1 m-0">
+                    <CardTitle className="text-start px-4">
+                        최근 작업 기록
+                    </CardTitle>
+                    <CardContent className="h-full">
+                        <div className="h-full flex flex-col justify-between gap-2">
+                            {[...Array(4)].map((_, i) => (
+                                <Skeleton
+                                    key={i}
+                                    className="w-full flex-1 min-h-6 rounded-md m-0"
+                                />
+                            ))}
+                        </div>
+                    </CardContent>
+                </Card>
+            </div>
         </div>
     );
 }
